de4291fac920a5659508fdfb54d150fe857508fb
Luan Santos <santosluan@id.uff.br>
2023-08-08 19:12:37 -0300

    Create Example1.sql

---
+++ LearnSQL60min/Example1.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE test;
+DROP DATABASE test;
+CREATE DATABASE record_company;
+USE record_company;
+CREATE TABLE test (
+	test_column INT 
+);
+ALTER TABLE test
+ADD another_column VARCHAR(255);
+DROP TABLE test;
